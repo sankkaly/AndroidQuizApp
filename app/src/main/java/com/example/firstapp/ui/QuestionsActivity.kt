63d48138165feeb5e741188d7827fdc2355acf3e
SANKET  KALYANKAR <128054652+sankkaly@users.noreply.github.com>
2024-07-15 17:18:56 +0530

    added Ui to questions Activity

--- app/src/main/java/com/example/firstapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/example/firstapp/ui/QuestionsActivity.kt
@@ -1,14 +1,34 @@
 package com.example.firstapp.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firstapp.R
+import com.example.firstapp.model.Question
 import com.example.firstapp.utils.Constants
 
+
 class QuestionsActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textViewProgress: TextView
+    private lateinit var textViewQuestion: TextView
+    private lateinit var flagImage: ImageView
+
+    private lateinit var textViewOptionOne: TextView
+    private lateinit var textViewOptionTwo: TextView
+    private lateinit var textViewOptionThree: TextView
+    private lateinit var textViewOptionFour: TextView
+
+    private  val currentPosition = 1
+    private lateinit var questionsList: MutableList<Question>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,7 +39,34 @@ class QuestionsActivity : AppCompatActivity() {
             insets
         }
 
-        val questionsList = Constants.getQuestions()
+        progressBar = findViewById(R.id.progressBar)
+        textViewProgress = findViewById(R.id.text_view_progress)
+        textViewQuestion = findViewById(R.id.question_text_view)
+        flagImage = findViewById(R.id.image_flag)
+
+        textViewOptionOne = findViewById(R.id.text_view_option_one)
+        textViewOptionTwo = findViewById(R.id.text_view_option_two)
+        textViewOptionThree = findViewById(R.id.text_view_option_three)
+        textViewOptionFour = findViewById(R.id.text_view_option_four)
+
+
+        questionsList = Constants.getQuestions()
+        Log.d("QuestionSize", "${questionsList.size}")
+
+        setQuestion()
+
 
     }
+    private fun setQuestion () {
+        val question = questionsList[currentPosition - 1]
+        flagImage.setImageResource(question.image)
+        progressBar.progress = currentPosition
+        textViewProgress.text = "$currentPosition/${progressBar.max}"
+        textViewQuestion.text = question.question
+        textViewOptionOne.text = question.optionOne
+        textViewOptionTwo.text = question.optionTwo
+        textViewOptionThree.text = question.optionThree
+        textViewOptionFour.text = question.optionFour
+    }
+
 }
