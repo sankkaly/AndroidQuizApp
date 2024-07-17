6278c8eb05003fc729399b106a7260e3c85b8a6e
SANKET  KALYANKAR <128054652+sankkaly@users.noreply.github.com>
2024-07-17 16:06:58 +0530

    Worked on TextView border highlight functionality when clicked

--- app/src/main/java/com/example/firstapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/example/firstapp/ui/QuestionsActivity.kt
@@ -1,12 +1,17 @@
 package com.example.firstapp.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firstapp.R
@@ -14,7 +19,7 @@ import com.example.firstapp.model.Question
 import com.example.firstapp.utils.Constants
 
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var progressBar: ProgressBar
     private lateinit var textViewProgress: TextView
@@ -26,8 +31,12 @@ class QuestionsActivity : AppCompatActivity() {
     private lateinit var textViewOptionThree: TextView
     private lateinit var textViewOptionFour: TextView
 
+    private lateinit var checkButton: Button
+
     private  val currentPosition = 1
     private lateinit var questionsList: MutableList<Question>
+
+    private var selectedOptionPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +52,18 @@ class QuestionsActivity : AppCompatActivity() {
         textViewProgress = findViewById(R.id.text_view_progress)
         textViewQuestion = findViewById(R.id.question_text_view)
         flagImage = findViewById(R.id.image_flag)
+        checkButton = findViewById(R.id.button_check)
 
         textViewOptionOne = findViewById(R.id.text_view_option_one)
         textViewOptionTwo = findViewById(R.id.text_view_option_two)
         textViewOptionThree = findViewById(R.id.text_view_option_three)
         textViewOptionFour = findViewById(R.id.text_view_option_four)
+
+        textViewOptionOne.setOnClickListener(this)  // setting onClickListeners to text view and button
+        textViewOptionTwo.setOnClickListener(this)
+        textViewOptionThree.setOnClickListener(this)
+        textViewOptionFour.setOnClickListener(this)
+        checkButton.setOnClickListener(this)
 
 
         questionsList = Constants.getQuestions()
@@ -67,6 +83,66 @@ class QuestionsActivity : AppCompatActivity() {
         textViewOptionTwo.text = question.optionTwo
         textViewOptionThree.text = question.optionThree
         textViewOptionFour.text = question.optionFour
+
+        if (currentPosition == questionsList.size) {
+            checkButton.text = "FINISH"
+        }else{
+            checkButton.text = "CHECK"
+        }
+
+
+    }
+
+    private fun resetOptions(){
+        val options = mutableListOf<TextView>()
+        options.add(textViewOptionOne)
+        options.add(textViewOptionTwo)
+        options.add(textViewOptionThree)
+        options.add(textViewOptionFour)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOption (textView: TextView, selectedOptionNumber:Int){
+        resetOptions()
+
+        selectedOptionPosition = selectedOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this, R.drawable.selected_option_border_bg
+        )
+
+    }
+
+    override fun onClick(view: View?) {
+
+        when (view?.id){
+            R.id.text_view_option_one -> {
+                selectedOption(textViewOptionOne, 1)
+            }
+            R.id.text_view_option_two -> {
+                selectedOption(textViewOptionTwo, 2)
+            }
+            R.id.text_view_option_three -> {
+                selectedOption(textViewOptionThree, 3)
+            }
+            R.id.text_view_option_four -> {
+                selectedOption(textViewOptionFour, 4)
+            }
+            R.id.button_check -> {
+
+            }
+
+
+        }
     }
 
 }
